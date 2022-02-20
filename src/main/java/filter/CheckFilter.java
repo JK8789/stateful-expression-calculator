cd85945a0b63c24f704366ce13307a369fbb3870
Korotkova Julia <korotkovayuliav@gmail.com>
2022-02-20 18:08:10 +0200

    working project with debug messages

--- src/main/java/filter/CheckFilter.java
+++ src/main/java/filter/CheckFilter.java
@@ -1,16 +1,90 @@
 package filter;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ReadListener;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 //import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+class MultiReadHttpServletRequest extends HttpServletRequestWrapper {
+	private ByteArrayOutputStream cachedBytes;
+
+	public MultiReadHttpServletRequest(HttpServletRequest request) {
+		super(request);
+	}
+
+	@Override
+	public ServletInputStream getInputStream() throws IOException {
+		if (cachedBytes == null)
+			cacheInputStream();
+
+		return new CachedServletInputStream();
+	}
+
+	@Override
+	public BufferedReader getReader() throws IOException {
+		return new BufferedReader(new InputStreamReader(getInputStream()));
+	}
+
+	private void cacheInputStream() throws IOException {
+		/*
+		 * Cache the inputstream in order to read it multiple times. For convenience, I
+		 * use apache.commons IOUtils
+		 */
+		cachedBytes = new ByteArrayOutputStream();
+		IOUtils.copy(super.getInputStream(), cachedBytes);
+	}
+
+	/* An inputstream which reads the cached request body */
+	public class CachedServletInputStream extends ServletInputStream {
+		private ByteArrayInputStream input;
+
+		public CachedServletInputStream() {
+			/* create a new input stream from the cached request body */
+			input = new ByteArrayInputStream(cachedBytes.toByteArray());
+		}
+
+		@Override
+		public int read() throws IOException {
+			return input.read();
+		}
+
+		@Override
+		public boolean isFinished() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean isReady() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void setReadListener(ReadListener readListener) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+}
 
 //@WebFilter(filterName="CheckFilter", urlPatterns="/*")
 public class CheckFilter implements Filter {
@@ -24,12 +98,15 @@ public class CheckFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("This is pre-processing!");
+		System.out.println();
 
 		// pre-processing
+		// 1. check URI
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
+		MultiReadHttpServletRequest wrappedRequest = new MultiReadHttpServletRequest(req);
 		String uri = req.getRequestURI();
+		System.out.println("This is pre-processing! " + uri);
 		if (uri.length() < 7) {
 			resp.sendError(400, "Incorrect value of URI1: " + uri);
 			return;
@@ -38,7 +115,7 @@ public class CheckFilter implements Filter {
 			resp.sendError(400, "Incorrect value of URI2: " + uri);
 			return;
 		}
-		String paramOfUri = uri.substring(6); 
+		String paramOfUri = uri.substring(6);
 		boolean trueUri = false;
 		if (paramOfUri.equals("expression")) {
 			trueUri = true;
@@ -51,8 +128,176 @@ public class CheckFilter implements Filter {
 			resp.sendError(400, "Incorrect value of URI3: " + uri);
 			return;
 		}
-		System.out.println("This is pre-processing!");
-		chain.doFilter(req, response);
+
+		// 2.check expression
+		if (paramOfUri.equals("expression")) {
+			boolean check_Expression = processExpression(wrappedRequest, resp);
+			if (check_Expression == false) {
+				return;
+			}
+		}
+
+		// 3.check variable
+		if ((paramOfUri.length() == 1) && ((paramOfUri.charAt(0) >= 'a') && (paramOfUri.charAt(0) <= 'z'))) {
+			if (!processVariable(wrappedRequest, resp, paramOfUri.charAt(0))) {
+				return;
+			}
+		}
+
+		System.out.println("Pre-processing is finished!");
+		chain.doFilter(wrappedRequest, response);
+	}
+
+	// checking expression method
+	private static boolean processExpression(ServletRequest request, ServletResponse response) throws IOException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		String expression = inputStreamToString(req.getInputStream());
+		System.out.println("Expression IN Pre-processing: " + expression);
+		final boolean checking = checkExpression(expression);
+		if (checking == false) {
+			resp.sendError(400, "BAD format");
+		}
+		return checking;
+
+	}
+
+	// conversion method: InputStream To String
+	private static String inputStreamToString(InputStream inputStream) {
+		try (Scanner scanner = new Scanner(inputStream, "UTF-8")) {
+			return scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+		}
+	}
+
+	// Lexer method for checking expression
+	enum State {
+		initial, open_bracket, close_bracket, character, sign, digit, operation;
+	}
+
+	public static boolean checkExpression(String expression) {
+		System.out.println("This is checkExpression method: " + expression);
+		State state = State.initial;
+		int brackets = 0;
+		for (int i = 0; i < expression.length(); i++) {
+			System.out.print("state = ");
+			System.out.println(state);
+			char c = expression.charAt(i);
+			if (state == State.initial) {
+				if (c == '(') {
+					brackets += 1;
+					state = State.open_bracket;
+				} else if ((c >= 'a') && (c <= 'z')) {
+					state = State.character;
+				} else if ((c >= '0') && (c <= '9')) {
+					state = State.digit;
+				} else if (c == '-') {
+					state = State.sign;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (state == State.open_bracket) {
+				if ((c >= 'a') && (c <= 'z')) {
+					state = State.character;
+				} else if ((c >= '0') && (c <= '9')) {
+					state = State.digit;
+				} else if (c == '-') {
+					state = State.sign;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (state == State.character) {
+				if ((c == '-') || (c == '+') || (c == '*') || (c == '/')) {
+					state = State.operation;
+				} else if (c == ')') {
+					brackets -= 1;
+					state = State.close_bracket;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					System.out.println("return false");
+					return false;
+				}
+			} else if (state == State.digit) {
+				if (c == ')') {
+					brackets -= 1;
+					state = State.close_bracket;
+				} else if ((c == '-') || (c == '+') || (c == '*') || (c == '/')) {
+					state = State.operation;
+				} else if ((c >= '0') && (c <= '9')) {
+					state = State.digit;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (state == State.sign) {
+				if ((c >= '0') && (c <= '9')) {
+					state = State.digit;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (state == State.operation) {
+				if ((c >= '0') && (c <= '9')) {
+					state = State.digit;
+				} else if ((c >= 'a') && (c <= 'z')) {
+					state = State.character;
+				} else if (c == '(') {
+					brackets += 1;
+					state = State.open_bracket;
+				} else if (c == '-') {
+					state = State.sign;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			} else if (state == State.close_bracket) {
+				if ((c == '-') || (c == '+') || (c == '*') || (c == '/')) {
+					state = State.operation;
+				} else if (c == ' ') {
+					continue;
+				} else {
+					return false;
+				}
+			}
+			if (i == expression.length() - 1) {
+				if (brackets != 0) {
+					return false;
+				}
+			}
+
+		}
+		if ((state == State.operation) || (state == State.open_bracket) || (state == State.sign)) {
+			return false;
+		}
+		return true;
+	}
+
+	// checking Variable method
+	private static boolean processVariable(ServletRequest request, ServletResponse response, char variable_name)
+			throws IOException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		// HttpSession session = req.getSession();
+		String variable_value = inputStreamToString(req.getInputStream());
+		try {
+			int var_value = Integer.parseInt(variable_value);
+			System.out.println(" проверяем на диапазон var_value: " + var_value);
+			if ((var_value < -10000) || (var_value > 10000)) {
+				System.out.println("var_value in processVariable TOO BIG or too small  " + var_value);
+				resp.sendError(403, "Forbidden: too big or too small");
+				return false;
+			}
+		} catch (Exception e) {
+			System.out.println("Bad parsing string  " + variable_value + "  to int");
+		}
+		return true;
 	}
 
 	@Override
--- src/main/java/servlet/StatefulCalcServlet.java
+++ src/main/java/servlet/StatefulCalcServlet.java
@@ -13,133 +13,11 @@ import javax.servlet.http.HttpSession;
 import bsh.EvalError;
 import bsh.Interpreter;
 
-class Lexer {
-	enum State {
-		initial, open_bracket, close_bracket, character, sign, digit, operation;
-	}
-
-	State state;
-
-	public boolean checkExpression(String expression) {
-		state = State.initial;
-		int brackets = 0;
-		for (int i = 0; i < expression.length(); i++) {
-			if (state == State.initial) {
-				if (expression.charAt(i) == '(') {
-					brackets += 1;
-					state = State.open_bracket;
-				} else if ((expression.charAt(i) >= 'a') && (expression.charAt(i) <= 'z')) {
-					state = State.character;
-				} else if ((expression.charAt(i) >= '0') && (expression.charAt(i) <= '9')) {
-					state = State.digit;
-				} else if (expression.charAt(i) == '-') {
-					state = State.sign;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					// System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.open_bracket) {
-				if ((expression.charAt(i) >= 'a') && (expression.charAt(i) <= 'z')) {
-					state = State.character;
-				} else if ((expression.charAt(i) >= '0') && (expression.charAt(i) <= '9')) {
-					state = State.digit;
-				} else if (expression.charAt(i) == '-') {
-					state = State.sign;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					// System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.character) {
-				if ((expression.charAt(i) == '-') || (expression.charAt(i) == '+') || (expression.charAt(i) == '*')
-						|| (expression.charAt(i) == '/')) {
-					state = State.operation;
-				} else if (expression.charAt(i) == ')') {
-					brackets -= 1;
-					state = State.close_bracket;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.digit) {
-				if (expression.charAt(i) == ')') {
-					brackets -= 1;
-					state = State.close_bracket;
-				} else if ((expression.charAt(i) == '-') || (expression.charAt(i) == '+')
-						|| (expression.charAt(i) == '*') || (expression.charAt(i) == '/')) {
-					state = State.operation;
-				} else if ((expression.charAt(i) >= '0') && (expression.charAt(i) <= '9')) {
-					state = State.digit;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.sign) {
-				if ((expression.charAt(i) >= '0') && (expression.charAt(i) <= '9')) {
-					state = State.digit;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.operation) {
-				if ((expression.charAt(i) >= '0') && (expression.charAt(i) <= '9')) {
-					state = State.digit;
-				} else if ((expression.charAt(i) >= 'a') && (expression.charAt(i) <= 'z')) {
-					state = State.character;
-				} else if (expression.charAt(i) == '(') {
-					brackets += 1;
-					state = State.open_bracket;
-				} else if (expression.charAt(i) == '-') {
-					state = State.sign;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			} else if (state == State.close_bracket) {
-				if ((expression.charAt(i) == '-') || (expression.charAt(i) == '+') || (expression.charAt(i) == '*')
-						|| (expression.charAt(i) == '/')) {
-					state = State.operation;
-				} else if (expression.charAt(i) == ' ') {
-					continue;
-				} else {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			}
-			if (i == expression.length() - 1) {
-				if (brackets != 0) {
-					System.out.println("Error in " + expression);
-					return false;
-				}
-			}
-
-		}
-		if ((state == State.operation) || (state == State.open_bracket) || (state == State.sign)) {
-			System.out.println("Error in " + expression);
-			return false;
-		}
-		return true;
-	}
-
-}
-
 //@WebServlet("/calc/*")
 public class StatefulCalcServlet extends HttpServlet {
 
 	private static String Calc(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-		// String expr_origin = req.getParameter("expression");
 		HttpSession session = req.getSession();
 		String expr_origin = (String) session.getAttribute("expression");
 		System.out.println("       expr_origin " + expr_origin);
@@ -147,14 +25,13 @@ public class StatefulCalcServlet extends HttpServlet {
 		for (int i = 0; i < expr_origin.length(); ++i) {
 			char c = expr_origin.charAt(i);
 			if ((c >= 'a') && (c <= 'z')) {
-				String atr_value = (String) session.getAttribute(String.valueOf(c));
-				System.out.println("проверка atr_value : " + atr_value);
-				if (atr_value == null) {
-					resp.setStatus(409);
-					System.out.println("       setStatus 409 ");
+				String attribute_value = (String) session.getAttribute(String.valueOf(c));
+				System.out.println("проверка atr_value : " + attribute_value);
+				if (attribute_value == null) {
+					resp.sendError(409, "Conflict: lack of data");
 					return null;
 				}
-				char value = atr_value.charAt(0);
+				char value = attribute_value.charAt(0);
 				System.out.println("value of " + String.valueOf(c) + " is " + String.valueOf(value));
 
 				if ((value >= 'a') && (value <= 'z')) { // check if value is name of another variable
@@ -186,46 +63,32 @@ public class StatefulCalcServlet extends HttpServlet {
 	private static void processExpression(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		HttpSession session = req.getSession();
 		String expression = inputStreamToString(req.getInputStream());
-		Lexer lexer = new Lexer();
-		System.out.println("expression who checking " + expression);
-		boolean checking = lexer.checkExpression(expression);
-		if (checking == false) {
-			resp.sendError(400, "BAD format");
-			return;
-		} else
-			System.out.println("expression after checking is GOOD! " + expression);
 		if (session.getAttribute("expression") == null) {
 			resp.setStatus(201);
 			resp.addHeader("Location: ", "/calc/expression");
 		} else {
 			resp.setStatus(200);
 		}
-		System.out.println("gooing to put expression " + expression);
+		System.out.println("gooing to put expression \"" + expression + "\"");
 		session.setAttribute("expression", expression);
-		System.out.println("expression IS PUTED in session  " + expression);
+		System.out.println("Expression \"" + expression + "\" IS PUTED in session.");
 	}
 
 	private static void processVariable(HttpServletRequest req, HttpServletResponse resp, char variable_name)
 			throws IOException {
 		HttpSession session = req.getSession();
 		String variable_value = inputStreamToString(req.getInputStream());
-//		if ((variable_value.length() == 1) && (variable_value.charAt(0) >= 'a') && (variable_value.charAt(0) <= 'z')) {
-//			variable_value = (String) session.getAttribute(variable_value);
-//		} else {
+//		try {
 //			int var_value = Integer.parseInt(variable_value);
+//			System.out.println(" проверяем на диапазон var_value: " + var_value);
+//			if ((var_value < -10000) || (var_value > 10000)) {
+//				System.out.println("var_value in processVariable TOO BIG or too small  " + var_value);
+//				resp.sendError(403, "Forbidden: too big or too small");
+//				return;
+//			}
+//		} catch (Exception e) {
+//			System.out.println("Bad parsing string  " + variable_value + "  to int");
 //		}
-		try {
-			int var_value = Integer.parseInt(variable_value);
-			System.out.println("проверяем на диапазон var_value: " + var_value);
-			if ((var_value < -10000) || (var_value > 10000)) {
-				System.out.println("var_value in processVariable TOO BIG or too smal  " + var_value);
-				//resp.setStatus(403); // to add the reason
-				resp.sendError(403, "Forbidden: too big or too smal");
-				return;
-			}
-		} catch (Exception e) {
-			System.out.println("Bad parsing string  " + variable_value + "  to int");
-		}
 
 		if (session.getAttribute(String.valueOf(variable_name)) == null) {
 			resp.setStatus(201);
@@ -239,22 +102,20 @@ public class StatefulCalcServlet extends HttpServlet {
 
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println();
 		String uri = req.getRequestURI();
 		String paramOfUri = uri.substring(6);
 		if (paramOfUri.equals("expression")) {
 			processExpression(req, resp);
 		} else if ((paramOfUri.length() == 1) && ((paramOfUri.charAt(0) >= 'a') && (paramOfUri.charAt(0) <= 'z'))) {
 			processVariable(req, resp, paramOfUri.charAt(0));
-		} else {
-			resp.sendError(400, "Incorrect value of URI: " + uri);
 		}
 
 	}
 
 	private static String inputStreamToString(InputStream inputStream) {
-		Scanner scanner = new Scanner(inputStream, "UTF-8");
-		return scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+		try (Scanner scanner = new Scanner(inputStream, "UTF-8")) {
+			return scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+		}
 	}
 
 	@Override
@@ -268,19 +129,11 @@ public class StatefulCalcServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// GET resp with value of evaluated expression to /calc/result URI.
-		// return 200 status code and calculated integer value as a response body OR
-		// return 409 status code and reason of this error.
 		String res = Calc(req, resp);
 		if (res == null) {
-			resp.sendError(409, "Conflict: lack of data");
 			return;
 		}
 		PrintWriter writer = resp.getWriter();
 		writer.write(res);
 	}
-
-//	private void doFilter() {
-//
-//	}
 }
